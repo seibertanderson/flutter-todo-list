6846bd546546ccf870be24d4866d4f9a1b259ef0
Anderson Seibert <andersonseibert0@gmail.com>
2021-07-07 18:31:58 -0300

    ...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flutter-todo-list
  
+
+https://javiercbk.github.io/json_to_dart/
---
+++ todolist/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'models/Item.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  var items = <Item>[];
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+    items.add(Item(title: "Item 3", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        actions: <Widget>[],
+      ),
+      body: ListView.builder(
+          itemCount: widget.items.length,
+          itemBuilder: (BuildContext ctx, int index) {
+            final item = widget.items[index];
+
+            return Text(widget.items[index].title);
+          }),
+    );
+  }
+}
